d7b12ebce9f7a97037fffcc5beb12385992b974c
Fernando Torres Padilla <tp.fernando@gmail.com>
2023-05-24 20:17:04 -0300

    Add React Query pagination example

--- ejemplos/src/examples/reactQuery/Page.jsx
+++ ejemplos/src/examples/reactQuery/Page.jsx
@@ -1,23 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import List from './components/List'
 import './styles.css'
-import { useState } from 'react'
 
 const queryClient = new QueryClient()
 
 export const ReactQueryPage = () => {
-  const [isShown, setIsShown] = useState(false)
-
   return (
     <QueryClientProvider client={queryClient}>
       <div className='App'>
         <h1>¡Rick & Morty!</h1>
-
-        <button onClick={() => setIsShown(x => !x)}>
-          {isShown ? 'Ocultar' : 'Mostrar'}
-        </button>
-        {isShown && <List />}
-        {/* <List /> */}
+        <List />
       </div>
     </QueryClientProvider>
   )
--- ejemplos/src/examples/reactQuery/services/api.js
+++ ejemplos/src/examples/reactQuery/services/api.js
@@ -1,10 +1,10 @@
-export const getCharacters = () => {
-  return fetch('https://rickandmortyapi.com/api/character').then(resp =>
-    resp.json()
-  )
+export const getCharacters = page => {
+  return fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+    .then(resp => resp.json())
+    .catch(err => console.log(err))
 }
 
-export const getCharaterById = id => {
+export const getCharacterById = id => {
   return fetch(`https://rickandmortyapi.com/api/character/${id}`).then(resp =>
     resp.json()
   )
